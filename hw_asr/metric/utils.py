a7a1ae5e0b06578ae5a976752747576b31ac44a8
Maksim Nikolaev <33720039+maximkm@users.noreply.github.com>
2022-10-07 19:58:35 +0300

    fix wer cer bug

--- hw_asr/metric/utils.py
+++ hw_asr/metric/utils.py
@@ -1,14 +1,14 @@
 import editdistance
 
 
-def calc_cer(target_text, predicted_text) -> float:
+def calc_wer(target_text, predicted_text) -> float:
     words_target = target_text.split()
     if len(words_target) == 0:
         return 1.
     return editdistance.distance(words_target, predicted_text.split()) / len(words_target)
 
 
-def calc_wer(target_text, predicted_text) -> float:
+def calc_cer(target_text, predicted_text) -> float:
     if len(target_text) == 0:
         return 1.
     return editdistance.distance(target_text, predicted_text) / len(target_text)
